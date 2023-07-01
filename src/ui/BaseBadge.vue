1eac14e3c8f82f8ee6776d41e4aa48dcc28cf2eb
lisa-crowell <lisa.l.crowell@gmail.com>
2023-07-01 15:55:53 -0500

    add base components

---
+++ src/ui/BaseBadge.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  props:
+    ['type', 'title'],
+  computed: {
+    text() {
+      return this.title.toUpperCase();
+    }
+  }
+}
+</script>
+
+<template>
+  <span class="badge" :class="type">
+    {{text}}
+  </span>
+</template>
+
+<style scoped>
+.badge {
+  background-color: #ccc;
+  color: #252525;
+  border-radius: 30px;
+  padding: 0.5rem 1.5rem;
+  display: inline-block;
+  margin-right: 0.5rem;
+}
+
+.frontend {
+  background-color: #00388d;
+  color: white;
+}
+
+.backend {
+  background-color: #4b965a;
+  color: white;
+}
+
+.career {
+  background-color: #6cce88;
+  color: white;
+}
+</style>
---
+++ src/ui/BaseButton.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+  props: {
+    mode: {
+      type: String,
+      required: false,
+      default: null
+    },
+    link: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    to: {
+      type: String,
+      required: false,
+      default: '/'
+    }
+  }
+}
+</script>
+
+<template>
+  <button v-if="!link" :class="mode">
+    <slot></slot>
+  </button>
+  <router-link v-else :to="to" :class="mode">
+    <slot></slot>
+  </router-link>
+</template>
+
+<style scoped>
+button,
+a {
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font: inherit;
+  background-color: #0f83ad;
+  border: 1px solid #97daee;
+  color: white;
+  cursor: pointer;
+  border-radius: 30px;
+  margin-right: 0.5rem;
+  display: inline-block;
+}
+
+a:hover,
+a:active,
+button:hover,
+button:active {
+  background-color: #0a7d86;
+  border-color: #0f83ad;
+}
+
+.flat {
+  background-color: transparent;
+  color: #0f83ad;
+  border: none;
+}
+
+.outline {
+  background-color: transparent;
+  border-color: #0f83ad;
+  color: #0f83ad;
+}
+
+.flat:hover,
+.flat:active,
+.outline:hover,
+.outline:active {
+  background-color: #97daee;
+}
+</style>
