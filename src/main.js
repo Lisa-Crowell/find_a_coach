f84ac53f614866df64c209e4e70122a6e320feed
lisa-crowell <lisa.l.crowell@gmail.com>
2023-07-01 15:56:54 -0500

    add imports and components, add prop

--- src/main.js
+++ src/main.js
@@ -6,12 +6,17 @@ import App from './App.vue'
 import router from "./router";
 import store from "./store/index.js";
 
-// createApp(App).mount('#app')
-// createApp({}).mount('#app');
+import BaseCard from "@/ui/BaseCard.vue";
+import BaseButton from "@/ui/BaseButton.vue";
+import BaseBadge from "@/ui/BaseBadge.vue";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
--- src/router.js
+++ src/router.js
@@ -12,9 +12,13 @@ const router = createRouter({
     routes: [
         { path: '/', redirect: '/coaches' },
         { path: '/coaches', component: CoachList },
-        { path: '/coaches/:id', component: CoachDetail, children: [
+        { path: '/coaches/:id',
+            component: CoachDetail,
+            props:true,
+            children: [
             { path: 'contact', component: ContactCoach }
-                ]}, // /coaches/coachId/contact
+            ]
+        }, // /coaches/coachId/contact
         { path: '/register', component: CoachRegistration },
         { path: '/requests', component: RequestReceived },
         { path: '/:notFound(.*)', component: NotFound},
