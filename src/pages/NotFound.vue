0b7f16c1f0f36b48ed27a594cdf51f430ccf1e49
lisa-crowell <lisa.l.crowell@gmail.com>
2023-07-12 14:53:09 -0500

    add not found component

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,9 +1,17 @@
 <script setup>
 
+import BaseCard from "@/ui/BaseCard.vue";
 </script>
 
 <template>
-
+  <div>
+    <section>
+      <base-card>
+        <h2>Page not found</h2>
+        <p>Sorry, this page does not exist. Please check out our other <router-link to="/coaches">coaches</router-link>!</p>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <style scoped>
