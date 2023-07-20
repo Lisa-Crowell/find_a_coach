684e5b148fd99008aaa64a5c3bae183a1e51366d
lisa-crowell <lisa.l.crowell@gmail.com>
2023-07-20 11:43:00 -0500

    add authentication and format code

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,18 +1,32 @@
-<script setup>
-
-</script>
-
 <template>
-<header>
-  <nav>
-    <h1><router-link to="/">Find a Coach</router-link></h1>
-    <ul>
-      <li><router-link to="/coaches">All Coaches</router-link></li>
-      <li><router-link to="/requests">Requests</router-link></li>
-    </ul>
-  </nav>
-</header>
+  <header>
+    <nav>
+      <h1><router-link to="/">Find a Coach</router-link></h1>
+      <ul>
+        <li><router-link to="/coaches">All Coaches</router-link></li>
+        <li v-if="isLoggedIn">
+          <router-link to="/requests">Requests</router-link>
+        </li>
+        <li v-else>
+          <router-link to="/auth">Login</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
+
+<script>
+export default {
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    },
+    isCoach() {
+      return this.$store.getters['coaches/isCoach'];
+    },
+  },
+}
+</script>
 
 <style scoped>
 header {
--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -1,3 +1,36 @@
+<template>
+  <base-dialog :show="!!error" title="An error occurred, please try back later." @close="handleError">
+    <p>{{error}}</p>
+  </base-dialog>
+  <section>
+    <coach-filter @change-filter="setFilters"></coach-filter>
+  </section>
+  <section>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
+        <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+        <base-button link to="/register" v-if="isLoggedIn && !isCoach && !isLoading">Register as a Coach</base-button>
+      </div>
+      <div v-if="isLoading">
+        <base-spinner>Loading...</base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
+        <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found!</h3>
+    </base-card>
+  </section>
+</template>
+
 <script>
 import CoachItem from "@/components/coaches/CoachItem.vue";
 import CoachFilter from "@/components/coaches/CoachFilter.vue";
@@ -26,6 +59,9 @@ export default {
     };
   },
   computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    },
     isCoach() {
       return this.$store.getters['coaches/isCoach'];
     },
@@ -69,38 +105,6 @@ export default {
   }
 }
 </script>
-
-<template>
-  <base-dialog :show="!!error" title="An error occurred, please try back later." @close="handleError">
-    <p>{{error}}</p>
-  </base-dialog>
-  <section>
-    <coach-filter @change-filter="setFilters"></coach-filter>
-  </section>
-  <section>
-    <base-card>
-    <div class="controls">
-      <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-      <base-button link to="/register" v-if="!isCoach && !isLoading">Register as a Coach</base-button>
-    </div>
-      <div v-if="isLoading">
-        <base-spinner>Loading...</base-spinner>
-      </div>
-    <ul v-else-if="hasCoaches">
-      <coach-item
-      v-for="coach in filteredCoaches"
-      :key="coach.id"
-      :id="coach.id"
-      :first-name="coach.firstName"
-      :last-name="coach.lastName"
-      :rate="coach.hourlyRate"
-      :areas="coach.areas"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches found!</h3>
-  </base-card>
-  </section>
-</template>
 
 <style scoped>
 ul {
