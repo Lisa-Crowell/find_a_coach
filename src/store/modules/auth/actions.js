5d743f899b6bbbeca169ef31bfe24069d2ed1284
lisa-crowell <lisa.l.crowell@gmail.com>
2023-07-21 14:30:47 -0500

    add auto login functionality

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,27 +1,27 @@
 export default {
     async login(context, payload) {
-        const response = await fetch(`${process.env.VUE_APP_LOGIN_URL}`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
         });
-        context.commit('setAuth', { isAuth: true })
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            throw new Error(responseData.message || 'Failed to login. Please check your login info.');
-        }
     },
     async signup(context, payload) {
-        const response = await fetch(`${process.env.VUE_APP_SIGNUP_URL}`, {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        });
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = `${process.env.VUE_APP_LOGIN_URL}`;
+        if (mode === 'signup') {
+            url = `${process.env.VUE_APP_SIGNUP_URL}`;
+        }
+
+        const response = await fetch(`${process.env.VUE_APP_AUTH_URL}`, {
             method: 'POST',
             body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
+                idToken: context.state.token
             })
         });
 
@@ -31,11 +31,26 @@ export default {
             throw new Error(responseData.message || 'Failed to authenticate. Please check your info and try again.');
         }
 
+        localStorage.setItem('token', responseData.idToken);
+        localStorage.setItem('userId', responseData.localId);
+
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
             tokenExpiration: responseData.expiresIn
         });
+    },
+    autoLogin(context) {
+      const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+
+        if (token && userId) {
+            context.commit('setUser', {
+                token: token,
+                userId: userId,
+                tokenExpiration: null
+            });
+        }
     },
     logout(context) {
         context.commit('setUser', {
